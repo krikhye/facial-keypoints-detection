2a1b9cf72749464853c1f501b6c6729ff0f6caab
Kai Rikhye <krikhye@gmail.com>
2013-08-06 10:56:07 -0400

    functions to process initial dataset

--- detection.r
+++ detection.r
@@ -1,6 +1,27 @@
-train.file <- paste0('training.csv')
-test.file <- paste0('test.csv')
+library(doMC)
+registerDoMC()
 
-d.train <- read.csv(train.file, stringsAsFactors=F)
+seperateImages <- function(d) {
+  im <- foreach(i = d$Image, .combine = rbind) %dopar% {
+    as.integer(unlist(strsplit(i, " ")))
+  }
+  d$Image <- NULL
+  return(im)
+}
 
-im.train <- 
+saveData <- function() {
+ train.file <- paste0('training.csv')
+ test.file <- paste0('test.csv')
+
+ d.train <- read.csv(train.file, stringsAsFactors=F)
+ im.train <- seperateImages(d.train)
+ 
+ d.test <- read.csv(test.file, stringsAsFactors=F)
+ im.test <- seperateImages(d.test)
+
+ save(d.train, im.train, d.test, im.test, file='data.Rd')
+}
+
+ 
+ 
+ 
